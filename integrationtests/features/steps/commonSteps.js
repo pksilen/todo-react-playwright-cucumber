d5b2272710e482a80f90455fb88cfc5ad34497e4
Petri Silen <petri.silen@nokia.com>
2024-06-04 14:54:41 +0300

    Update

--- integrationtests/features/steps/commonSteps.js
+++ integrationtests/features/steps/commonSteps.js
@@ -1,15 +1,31 @@
 import { After, When } from '@cucumber/cucumber';
-import { chromium } from '@playwright/test';
+import { chromium, firefox, webkit } from '@playwright/test';
 import TodoAppPage from './TodoAppPage.js';
 
-When('user navigates to application', async function () {
+When('user navigates to application using chromium', async function () {
   this.browser = await chromium.launch({ headless: false });
   const context = await this.browser.newContext();
   this.page = await context.newPage();
   this.todoAppPage = new TodoAppPage(this.page);
-  this.todoAppPage.goto();
+  await this.todoAppPage.goto();
+});
+
+When('user navigates to application using firefox', async function () {
+  this.browser = await firefox.launch({ headless: false });
+  const context = await this.browser.newContext();
+  this.page = await context.newPage();
+  this.todoAppPage = new TodoAppPage(this.page);
+  await this.todoAppPage.goto();
+});
+
+When('user navigates to application using webkit', async function () {
+  this.browser = await webkit.launch({ headless: false });
+  const context = await this.browser.newContext();
+  this.page = await context.newPage();
+  this.todoAppPage = new TodoAppPage(this.page);
+  await this.todoAppPage.goto();
 });
 
 After(async function () {
-  await this.browser?.close();
+  await this.browser.close();
 });
--- integrationtests/features/windowTitle.feature
+++ integrationtests/features/windowTitle.feature
@@ -1,6 +1,12 @@
 Feature: Window title
 
-  Scenario: Window title shows application name
-    When user navigates to application
+  Scenario Outline: Window title shows application name
+    When user navigates to application using <browser>
     Then application name is shown in window title
+
+    Examples:
+      | browser  |
+      | chromium |
+      | firefox  |
+      | webkit   |
 
